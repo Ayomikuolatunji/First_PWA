4ae01eeffde5b546cd5e6046bfdaf325fb8ac5a0
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-15 21:54:23 +0100

    working with service workers and installable functionality

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,28 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+  if (eventPrompt) {
+    eventPrompt.prompt();
+    eventPrompt.userChoice.then(function (choice) {
+      if (choice === "dismissed") {
+        console.log("user canceled installation",choice.outcome);
+      } else {
+        console.log("user added to home screen",choice.outcome);
+      }
+    });
+    eventPrompt=null 
+  }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
