a9af6bca47ea1c39b382421cafc90c871a600c6e
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-15 21:47:37 +0100

    working with service workers and installable functionality

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,17 @@
+let eventPrompt;
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(function() {
-      console.log('Service worker registered!');
-    });
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function () {
+    console.log("Service worker registered!");
+    window.addEventListener("beforeinstallprompt", function(event){
+       e.preventDefault()
+       console.log("object");
+       eventPrompt=event
+       return false
+    })
+  })
+  .catch(function(error){
+     console.log(error);
+  })
 }
