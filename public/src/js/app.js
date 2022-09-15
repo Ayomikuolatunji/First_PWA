13dfc27dc9435bdcb293e8eb6f9aaf1c22936cb2
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-15 21:15:24 +0100

    working with service workers

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,34 +1,8 @@
-// if("serviceWorker" in  navigator){
-//     navigator.serviceWorker.register("../../sw.js")
-//     .then(()=>{
-//         console.log("serviceWorker registered")
-//         self.addEventListener("install", function(){
-//             console.log("installing service worker", e);
-//             return self.clients.claim()
-//         })
-        
-//         self.addEventListener("activate", function(){
-//             console.log("activating service worker", e);
-//             return self.clients.claim()
-//         })
-//     })
-//     .catch(err=>{
-//         console.log(err)
-//     })
-// }
 
-
-
-self.addEventListener('install', function(event) {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-  });
-  
-  self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ...', event);
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something ....', event);
-    event.respondWith(fetch(event.request));
-  });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
+    });
+}
