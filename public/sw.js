54798ba4e327d6e0c3c2f48dee96661ceee8eacb
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-17 10:32:05 +0100

    working with cache api

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,34 @@
-
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-   event.waitUntil(caches.open("static").then(function(cache){
-      console.log("[Cache all static assests]")
-      cache.add("./src/js/app.js")
-   }).catch(error=>{
-      console.log(error)
-   }))
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches
+      .open("static")
+      .then(function (cache) {
+        console.log("[Cache all static assests]");
+        cache.add("./src/js/app.js");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function(res){
-       if(res){
-        
-       }
-    })
-    .catch(error=>{
-
-    })
+    caches
+      .match(event.request)
+      .then(function (res) {
+        if (res) {
+          return res;
+        } else {
+          return fatch(event.request);
+        }
+      })
+      .catch((error) => {})
   );
 });
