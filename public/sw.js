b995d0a0bcb8902ffebf8ffb6b4aa588d11a53ef
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-17 11:28:14 +0100

    working with cache api

--- public/sw.js
+++ public/sw.js
@@ -18,7 +18,8 @@ self.addEventListener("install", function (event) {
           "./src/css/help.css",
           "./src/images/main-image.jpg",
           "https://fonts.googleapis.com/css?family=Roboto:400,700",
-          "https://fonts.googleapis.com/icon?family=Material+Icons"
+          "https://fonts.googleapis.com/icon?family=Material+Icons",
+          "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
         ]);
       })
       .catch((error) => {
